9f3a3a18d39a698557939f972552a48d79cab6db
64Noi <70427101+64Noi@users.noreply.github.com>
2020-09-12 00:26:20 +0900

    Add files via upload

---
+++ Bot.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!') 
+
+@bot.event 
+async def on_ready():
+    print("실행 완료")
+
+@bot.command()
+async def 청소(ctx, amount : int):
+    await ctx.channel.purge(limit=amount)
+
+@bot.event
+async def on_message(message):
+    message_content = message.content
+    bad = message_content.find("시발")
+    print(bad)
+    if bad >= 0:
+        await message.channel.send("고운 말을 사용해주세요.")
+        await message.delete()
+    await bot.process_commands(message)
+
+bot.run("NzQwMDkxNTAwNjMyOTk3OTEx.Xyj96w.LN5sHtV1BBHfFrL1MLeT61IzmSk")
